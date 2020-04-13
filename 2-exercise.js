07168bc573abfae546d7649aaca9cfeb47681981
Eric <eric@vicdna.com>
2020-04-13 17:27:25 +0800

    backup

--- 2-exercise.js
+++ 2-exercise.js
@@ -1,5 +1,6 @@
 var header = document.querySelector('header');
-var section = document.querySelector('section');
+var section1 = document.querySelector('#section_1');
+var section2 = document.querySelector('#section_2');
 
 var requestURL = 'https://opendata.cwb.gov.tw/api/v1/rest/datastore/F-C0032-001?Authorization=CWB-E3A1078B-3C53-44E9-BF80-E28DCFBB65FB&locationName=%E6%96%B0%E5%8C%97%E5%B8%82';
 var request = new XMLHttpRequest();
@@ -28,11 +29,12 @@ function showMeteorological(jsonObj) {
     var elementCI = weather['weatherElement'][3];
     var elementMaxT = weather['weatherElement'][4];
 
-    var myArticle = document.createElement('article');
     var myH2 = document.createElement('h2');
+    var myArticle1 = document.createElement('article');
+    var myArticle2 = document.createElement('article');
 
     myH2.textContent = locationNameString;
-    myArticle.appendChild(myH2);
+    myArticle1.appendChild(myH2);
 
     for (let i = 0; i < elementWx.time.length; i++) {
 
@@ -48,7 +50,7 @@ function showMeteorological(jsonObj) {
         time1.textContent = elementWx.time[i].startTime;
         time2.textContent = elementWx.time[i].endTime;
         p1.textContent = elementWx.time[i].parameter.parameterName;
-        span1.textContent = elementWx.time[i].parameter.parameterValue;
+        // span1.textContent = elementWx.time[i].parameter.parameterValue;
 
         // Step3：將元素加至網頁
         cardBody.appendChild(time1);
@@ -57,7 +59,7 @@ function showMeteorological(jsonObj) {
         cardBody.appendChild(span1);
 
         card.appendChild(cardBody);
-        myArticle.appendChild(card);
+        myArticle1.appendChild(card);
 
         // Step4：加入 class name
         cardBody.classList.add("card-body");
@@ -67,7 +69,7 @@ function showMeteorological(jsonObj) {
 
     for (let i = 0; i < elementPoP.time.length; i++) {
 
-        // Step1：創建 html 元素
+        // Step1：創建 html DOM 元素
         var cardBody = document.createElement('div');
         var card = document.createElement('div');
         var time1 = document.createElement('time');
@@ -75,10 +77,10 @@ function showMeteorological(jsonObj) {
         var p1 = document.createElement('p');
         var span1 = document.createElement('span');
 
-        // Step2：元素中加入資料內容
+        // Step2：在 DOM 元素中加入資料內容
         time1.textContent = elementPoP.time[i].startTime;
         time2.textContent = elementPoP.time[i].endTime;
-        p1.textContent = elementPoP.time[i].parameter.parameterName;
+        p1.textContent = "下雨機率" + elementPoP.time[i].parameter.parameterName + "%";
         span1.textContent = elementPoP.time[i].parameter.parameterValue;
 
         // Step3：將元素加至網頁
@@ -88,7 +90,7 @@ function showMeteorological(jsonObj) {
         cardBody.appendChild(span1);
 
         card.appendChild(cardBody);
-        myArticle.appendChild(card);
+        myArticle2.appendChild(card);
 
         // Step4：加入 class name
         cardBody.classList.add("card-body");
@@ -96,5 +98,6 @@ function showMeteorological(jsonObj) {
 
     }
 
-    section.appendChild(myArticle);
+    section1.appendChild(myArticle1);
+    section2.appendChild(myArticle2);
 }
